2160d812312808293df9f1dcd12f2e0e7196d4b2
belhab8 <belhabri.corentin@gmail.com>
2026-04-14 14:14:02 +0200

    Création de la fonction clean_2014 pour nettoyer le dataset de 2014

---
+++ cleaning_2014.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+data = pd.read_csv('./data/WorldCup.csv',sep=';')
+
+def clean_2014(data) :
+    # Certains matchs en double, on les supprime :
+    data = data.drop_duplicates()
+
+    # Normalisation de la date au format YYYYMMDD :
+    data['Datetime'] = pd.to_datetime(data['Datetime']).dt.strftime("%Y%m%d")
+
+    # Création de la colonne résultat
+    data['result'] = 'draw'
+    data.loc[data['Home Team Goals'] > data['Away Team Goals'], 'result']= 'home_team'
+    data.loc[data['Home Team Goals'] < data['Away Team Goals'], 'result']= 'away_team'
+
+    # Normalisation des Rounds
+    data.loc[data['Stage'].str.startswith('Group'),'Stage'] = 'Poules'
+    data.loc[data['Stage'] == 'Round of 16','Stage'] = 'Huitièmes de finale'
+    data.loc[data['Stage'] == 'Quarter-finals','Stage'] = 'Quarts de finale'
+    data.loc[data['Stage'] == 'Semi-finals','Stage'] = 'Demi-finales'
+    data.loc[data['Stage'] == 'Play-off for third place','Stage'] = 'Troisième place'
+    data.loc[data['Stage'] == 'Final','Stage'] = 'Finale'
+    return pd.DataFrame({
+        'id_match':data['MatchID'],
+        'home_team': data['Home Team Name'],
+        'away_team': data['Away Team Name'],
+        'home_result': data['Home Team Goals'],
+        'away_result': data['Away Team Goals'],
+        'result': data['result'],
+        'date': data['Datetime'],
+        'round': data['Stage'],
+        'city': data['City'],
+        'edition': data['Year']
+    }).sort_values(['date','id_match'])
+
+print(clean_2014(data))
